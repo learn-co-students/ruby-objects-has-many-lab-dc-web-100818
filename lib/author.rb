41eb89358f29e5d8ad118ec934063b5380a3a381
github username <github email address>
2018-10-15 15:04:50 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,41 @@
+class Author
+	attr_accessor :name
+
+	@@all = []
+
+	def initialize(name)
+		@name = name
+	end
+
+	def self.all
+		@@all
+	end
+
+	def posts
+		Post.all.select {|post| post.author == self}
+
+		#binding.pry
+	end
+
+	def add_post(post)
+
+		
+		post.author = self
+		
+		#binding.pry
+		#puts "hi"
+	end
+
+	def add_post_by_title(title)
+		post = Post.new(title)
+		post.author = self
+		#binding.pry
+		#puts "hi"
+	end
+
+	def self.post_count
+		Post.all.count
+	end
+
+
+end
